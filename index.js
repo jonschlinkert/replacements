560effcc75b0b9963377a990e90a42ebc040b38b
jonschlinkert <jon.schlinkert@sellside.com>
2015-03-13 09:08:18 -0400

    simplify

--- index.js
+++ index.js
@@ -1,34 +1,26 @@
 /*!
  * replacements <https://github.com/jonschlinkert/replacements>
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
- * Licensed under the MIT License
+ * Copyright (c) 2014-2015, Jon Schlinkert.
+ * Licensed under the MIT License.
  */
 
 'use strict';
 
-module.exports = function replacements(str, arr) {
-  var transformers = [];
+module.exports = function replacements(str, val) {
+  if (typeof str !== 'string') {
+    throw new TypeError('replacements expects a string.');
+  }
 
-  // normalize to an array
-  arr = !Array.isArray(arr) ? [arr] : arr;
-  arr.forEach(function(pair) {
-    if (!pair.hasOwnProperty('pattern')
-      && !pair.hasOwnProperty('replacement')) {
-      var keys = Object.keys(pair);
-      keys.forEach(function (key) {
-        var value = pair[key];
-        transformers.push({
-          pattern: key,
-          replacement: value
-        });
-      });
-    } else {
-      transformers.push(pair);
-    }
-  });
-
-  return transformers.reduce(function(acc, obj) {
-    return acc.replace(obj.pattern, obj.replacement);
-  }, str);
+  var arr = arrayify(val);
+  var len = arr.length, i = 0;
+  while (len--) {
+    var obj = arr[i++];
+    str = str.replace(obj.pattern, obj.replacement);
+  }
+  return str;
 };
+
+function arrayify (val) {
+  return !Array.isArray(val) ? [val] : val;
+}
